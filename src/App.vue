706132d6a67c2129690e0dda5ce62eaf95608f89
zjz_Service <1779072432@qq.com>
2018-02-06 09:35:53 +0800

    添加悬浮窗，有点bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <v-header></v-header>
-    <div class="tab">
+    <v-header v-bind:seller="seller"></v-header>
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -15,23 +15,42 @@
     <router-view ></router-view>
   </div>
 </template>
-<script>
+<script type="text/ecmascript-6">
   import header from './components/header/header';
-export default {
-  name: 'App',
-  components: {
-    'v-header': header
-  }
-};
+  const ERR_OK = 0;
+  export default {
+    name: 'App',
+    components: {
+      'v-header': header
+    },
+    data () {
+      return {
+        seller: {}
+      };
+    },
+    created() {
+      // 使用=>函数没事，用function（response）{ }就会报错。。。。
+      this.$http.get('/api/seller').then((response) => {
+        response = response.data;
+        console.log(response);
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+          console.log(this.seller);
+        }
+      });
+    }
+  };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
   #app
     .tab
       display: flex
       width:100%
       height: 40px
       line-height: 40px
-      border-bottom: 1px solid rgba(7,17,27,0.1)
+      //border-bottom: 1px solid rgba(7,17,27,0.1)
+      border-1px(rgba(7,17,27,0.1))
       .tab-item
         flex: 1
         text-align: center
