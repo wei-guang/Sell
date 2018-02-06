be914d2d080daf0b6b57b05ddcb3faf9ab0d6fb0
zjz_Service <1779072432@qq.com>
2018-02-06 17:34:42 +0800

    模拟获取后端商品信息，get成功，回家加班，手动滑稽

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,48 @@
 <template>
-    <div>i am goods</div>
+    <div class="goods">
+      <div class="menu-wrapper"></div>
+      <div class="foods-wrapper"></div>
+    </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        name: 'goods'
-    };
+  const ERR_OK = 0;
+  export default {
+      name: 'goods',
+      props: {
+        seller: {
+          type: Object
+        }
+      },
+      data: function () {
+        return {
+          goods: {}
+        };
+      },
+      created: function() {
+        this.$http.get('api/goods').then((response) => {
+          response = response.data;
+          if (response.errno === ERR_OK) {
+            this.goods = response.data;
+            console.log(this.goods);
+          }
+          });
+      }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .goods
+    display: flex
+    position: absolute
+    width: 100%
+    top: 174px
+    bottom: 46px
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px //等分，内容缩放，站位空间】
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 0 0 80px
 </style>
